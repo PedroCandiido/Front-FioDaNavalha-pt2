f355a773c492eef5e2670c4dfff05bd72a4f8064
PedroCandiido <116935967+PedroCandiido@users.noreply.github.com>
2022-10-29 12:18:03 -0300

    Update rotas

--- src/app/about-us/about-us.component.ts
+++ src/app/about-us/about-us.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-about-us',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutUSComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
-  ngOnInit(): void {
+  ngOnInit(){
   }
+
+  goToSobre() {
+    this.router.navigate(['/sobre']);
+}
 
 }
